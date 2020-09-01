8b44aa4facf892c58a55a2245472c30f091562bf
Hamza <hamzayousuf175@gmail.com>
2020-09-01 06:02:35 +0500

    cart comp finshed and nav bar also updated

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ProductsService } from "./../../Services/Products/products.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
-import { UserService } from './../../Services/User/user.service';
+import { UserService } from "./../../Services/User/user.service";
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
   public isAuthenticated;
+  public itemCount: number;
   ngOnInit() {
-    this.isAuthenticated = this.userService.isAuthenticated
+    this.isAuthenticated = this.userService.isAuthenticated;
     this.userService.getAuthStatusListner().subscribe((isAuthenticated) => {
-      console.log("....?",this.userService.isAuthenticated)
+      console.log("....?", this.userService.isAuthenticated);
       this.isAuthenticated = isAuthenticated;
+    });
+
+    //item count
+    this.itemCount = 0; //init it will b zeroi
+    this.productsService.getUpdatedOrderList().subscribe((tempObj) => {
+      this.itemCount = tempObj.orderProducts.length;
     });
   }
 
   logout() {
     this.userService.logout();
-    this.router.navigate(['/']);
+    this.router.navigate(["/"]);
   }
 }
--- src/app/user-cart/user-cart.component.ts
+++ src/app/user-cart/user-cart.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductsService } from "./../Services/Products/products.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user-cart',
-  templateUrl: './user-cart.component.html',
-  styleUrls: ['./user-cart.component.css']
+  selector: "app-user-cart",
+  templateUrl: "./user-cart.component.html",
+  styleUrls: ["./user-cart.component.css"],
 })
 export class UserCartComponent implements OnInit {
-
-  constructor() { }
+  constructor(private productsService: ProductsService) {}
+  public cartProduct;
+  public totalPrice;
 
   ngOnInit() {
+    this.cartProduct = this.productsService.orderProducts;
+    this.totalPrice = this.productsService.totalPrice;
+    this.productsService.getUpdatedOrderList().subscribe((temp) => {
+      console.log("what are we getting in the cart arr", temp);
+      this.cartProduct = temp.orderProducts;
+      this.totalPrice = temp.totalPrice;
+      console.log(this.totalPrice)
+    });
   }
 
+  placeOrder(){
+    //CALL HTTP SERVICE
+    alert("TODO")
+  }
 }
